62650c42a31116d470960421e2d465212448843f
Pablo Porzio <pabloporzio97@gmail.com>
2024-11-29 17:16:46 -0300

    Initial Commit

---
+++ Pick-a-Pal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pick-a-Pal
+//
+//  Created by Pablo Porzio (Modak) on 29/11/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
